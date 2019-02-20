ee1741143bca96d55c838a21a5ffbb0d513ab75c
subhankardas90 <das.subhankar90@gmail.com>
2019-02-20 16:12:07 +0530

    second commit

---
+++ course-2/src/components/UserOutput.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const UserOutput = () => {
+    return <div>
+        <p> Subhankar Das started learning react</p>
+        <p> Thank you</p>
+    </div>;
+}
+ 
+export default UserOutput;
